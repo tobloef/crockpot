7216ee1e4051a777e4b9d060d1a92bf77a5b4518
Tobias Løfgren <tobloef@gmail.com>
2025-02-12 22:43:45 +0100

    Add `NOTE` comment about disjoint sets

--- src/query/create-plan.ts
+++ src/query/create-plan.ts
@@ -9,5 +9,12 @@ export function createPlan(
   slots: QuerySlots,
   graph: Graph
 ): QueryPlan {
-
+  // NOTE: Identity disjoint sets of the input graph
+  //   For every slot, check if it's in one of the sets
+  //   If it is, great, this is the set we're going with.
+  //   If it is not, add it to a new set.
+  //   For every connection is has, check that slot as well.
+  //   If the slot is in the same set, great, we're good.
+  //   If not, add it to the current set.
+  //   If another set has that slot as well, merge the two sets.
 }
