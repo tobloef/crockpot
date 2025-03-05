6c965b733795303fd28c1a37edd6f4f1f3e1888d
Tobias Løfgren <tobloef@gmail.com>
2025-03-05 22:42:16 +0100

    Actually, use v1.4.0 as the version number

---
+++ dist/query/run-query.types.js
@@ -0,0 +1,3 @@
+import { NamedRelatedNodeQueryItem, NodeQueryItem, RelatedNodeQueryItem } from "../node/node-query-item.js";
+import { EdgeQueryItem } from "../edge/edge-query-item.js";
+//# sourceMappingURL=run-query.types.js.map
