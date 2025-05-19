737405b90622fb2f90f03e793566195d59ae7d46
Tobias Løfgren <tobloef@gmail.com>
2025-05-19 21:14:47 +0200

    tmp

--- v2/index.ts
+++ v2/index.ts
@@ -1,60 +1,6 @@
 // noinspection JSUnresolvedReference
 // language=JavaScript
 `
-	// Spaceship example
-
-	query()
-		.match(
-			Spaceship("ship")
-				.with(Docked)
-				.to(Planet)
-				.with(RuledBy)
-				.to(Faction)
-				.with(Allied)
-				.to(Faction)
-				.with(IsIn)
-				.from("ship")
-		)
-		.return("ship");
-
-	query()
-		.match(Spaceship("ship"))
-		.with(Docked)
-		.to(Planet)
-		.with(RuledBy)
-		.to(Faction)
-		.with(Allied)
-		.to(Faction)
-		.with(IsIn)
-		.from("ship")
-		.return("ship");
-
-	query()
-		.match(
-			Spaceship("ship"),
-			has(Docked),
-			to(Planet),
-			has(RuledBy),
-			to(Faction),
-			has(Allied),
-			to(Faction),
-			has(IsIn),
-			from("ship"),
-		)
-		.return("ship");
-
-	query()
-		.match(
-			Spaceship("ship")
-				.with(Docked, to, Planet)
-				.with(RuledBy, to, Faction)
-				.with(Allied, to, Faction)
-				.with(IsIn, from, "ship")
-		)
-		.return("ship");
-
-	// Global transforms example
-
 	query()
 		.match(
 			GlobalTransform("global")
@@ -129,90 +75,62 @@
 				.with(IsGlobalOf, from, GlobalTransform.as("parentGlobal"))
 		)
 		.return("local", "global", "parentGlobal");
-
-	// Velocity example
-
-	query()
-		.match(
-			Node.withAll(
-				Edge.to(Position("pos")),
-				Edge.to(Velocity("vel")),
-			)
-		).return("pos", "vel");
-
-	query()
-		.match(
-			Node.withAll(
-				[ Edge, to, Position("pos") ],
-				[ Edge, to, Velocity("vel") ],
-			)
-		).return("pos", "vel");
-
-	query()
-		.match(
-			Node.with(
-				[ Edge, to, Position("pos") ],
-				[ Edge, to, Velocity("vel") ],
-			)
-		).return("pos", "vel");
-
-	query()
-		.match(
-			Node.toAll(
-				Position.as("pos"),
-				Velocity.as("vel"),
-			)
-		).return("pos", "vel");
-
-	query()
-		.match(
-			Node.to(
-				Position("pos"),
-				Velocity("vel"),
-			)
-		).return("pos", "vel");
 `;
 
-/*
-Takeaways:
-
- I don't like the `.maybe.foo()` syntax. Just use `.maybeFoo()`.
-
- Similarly, the `Node("name")` syntax also has poor discoverability.
-
- The `.with(edge, direction, node)` syntax is really nice, actually.
-
- I like `.withOptional()` more than `.maybeWith()`, it reads nicer together with the `.with()`.
-
- Just keep it simple and use `.withAll()` instead of overloading `.with()`.
-
- I don't know what to think of having multiple match/optional terms yet.
- On the one hand, it is a little easier to reason about. You can just kinda declare
- "I have this thing, this thing and this thing, and they are related in these ways",
- without having to think much about the order.
- On the other hand, it creates the potential for creating disjoint queries.
- You would have to check at parse-time if that was the case, and reject the query if so.
- Assuming that you don't want to deal with disjoint query parts of course.
- */
+////////////////////////////////////////////
 
 graph
-	.match(
-		Spaceship.as("ship")
-			.with(Docked).to(Planet.as("planet"))
-			.with(RuledBy).from(Faction.as("faction"))
-			.withAll(
-				Allied.as("allied").to(Faction.as("faction")),
-				IsIn.as("isIn"),
-			),
-	);
+	.query(
+		Node.toAll(
+			Position.as("pos"),
+			Velocity.as("vel"),
+		)
+	)
+	.return("pos", "vel");
 
 graph
-	.match(
+	.query(
 		Spaceship.as("ship")
 			.with(Docked.to(Planet.as("planet")))
-			.with(RuledBy.from(Faction.as("faction")))
-			.withAll(
-				Allied.as("allied").to(Faction.as("faction")),
-				IsIn.as("isIn"),
-			),
-	);
+			.with(RuledBy.to(Faction.as("planet rulers")))
+			.with(Allied.to(Faction.as("ship owners")))
+			.with(OwnedBy.from("ship", "ship owners"))
+	)
+	.return("ship");
+
+////////////////////////////////////////////
+
+graph
+	.query(
+		Node("n"),
+		Position("pos"),
+		Velocity("vel"),
+		Edge("n", "pos"),
+		Edge("n", "vel"),
+	)
+	.return("pos", "vel");
+
+graph
+	.query(
+		Spaceship("ship"),
+		Planet("planet"),
+		Faction("ship owners"),
+		Faction("planet rulers"),
+		OwnedBy("ship", "ship owners"),
+		RuledBy("planet", "planet rulers"),
+		Allied("planet rulers", "ship owners"),
+	)
+	.return("ship");
+
+graph
+	.query(
+		LocalTransform("local"),
+		GlobalTransform("global"),
+		IsGlobalOf("global", "local"),
+		optional(
+			GlobalTransform("parentGlobal"),
+			LocalTransform("parentLocal"),
+			IsParentOf("parentLocal", "local"),
+		)
+	)
+	.return("local", "global", "parentGlobal");
